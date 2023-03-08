2e403424769b619390b8630765352912cd598ff2
wai moe <waimoe19286@gmail.com>
2023-03-08 07:23:18 -0800

    two

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const fetchData = async () => {
   const url = localStorage.getItem("apiUrl");
   if (url) {
-    const response = await fetch(`${url}/api/user`);
+    const response = await fetch(`${url}/user`);
     const data = await response.json();
     console.log(data);
   } else {
-    window.location.href = "/wai";
+    window.location.href = "/api";
   }
 };
 fetchData();
