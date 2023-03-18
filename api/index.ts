7bb85d632d25ad3752c3fd572d8a6e2cb6f1ca5c
wai moe <waimoe19286@gmail.com>
2023-03-18 03:50:42 -0700

    l

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import bodyParser from "body-parser"; //post ကနေ data လိုချင်ရင်သုံးတာ
+import formidable from "formidable"; // ပုံတွေကိုတစ်ပုံထက်ပို save ချင်ရင်သုံးတာ
+import { v4 as uuidv4 } from "uuid"; // redom  string လိုချင်လို့ သုံးတာ
 const app = express();
 dotenv.config();
-const port = 3000;
+const port = 3155;
 const apiUrl = process.env.API_URL;
 
 app.use(express.static("public"));
-
+app.use(bodyParser.json());
 const html = `
 <!DOCTYPE html>
 <html>
@@ -28,6 +32,25 @@ app.get("/api", (req: Request, res: Response) => {
 app.get("/api/users", (req: Request, res: Response) => {
   res.send(JSON.stringify({ name: "waimoe", age: "wai" }));
 });
-app.listen(3000, () => {
+
+app.post("/api/fileupload", (req: Request, res: Response) => {
+  const idwai = uuidv4();
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+      return res.end(String(err));
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ fields, files }, null, 2));
+  });
+  // console.log(req.headers);
+  // const writeStream = fs.createWriteStream("./test.jpg");
+  // req.pipe(writeStream);
+  // res.end();
+});
+
+app.listen(3155, () => {
   console.log("Sever listen ", port);
 });
--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
+const apiUrl = localStorage.getItem("apiUrl");
+
 const fetchData = async () => {
-  const apiUrl = localStorage.getItem("apiUrl");
   if (apiUrl) {
     const response = await fetch(`${apiUrl}/users`);
     const data = await response.json();
@@ -10,3 +11,16 @@ const fetchData = async () => {
 };
 
 fetchData();
+
+const fileUpload = async () => {
+  const filemoe = document.getElementById("upload");
+  const files = [...filemoe.files]; // array ပြောင်းချင်ရင်သုံးတာ
+  console.log(files);
+  const fromdata = new FormData();
+  files.forEach((file) => fromdata.append("files", file));
+  const waimoe = await fetch(`${apiUrl}/fileupload`, {
+    method: "POST",
+    body: fromdata,
+  });
+  console.log(waimoe);
+};
